ef2b96fb4e0ca81371e45de82edcd58155f60d1c
fuma <jisfteteos@gmail.com>
2021-11-25 21:55:25 +0900

    作業2日目終わり

--- 卒業制作a/app.py
+++ 卒業制作a/app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
-app.secret_key = "卒a"
 
+@app.route("/")
+def hello():
+    return "<p>ありがとう</p>"
+    
 @app.route("/dbtest")
 def dbtest():
-    on = sqlite3.connect("卒業制作.db")
+    con = sqlite3.connect("卒業制作.db")
     c = con.cursor()
     c.execute("SELECT name, address, image, comment, parking, toilet, playset, convenience, history, vending, plaza, water, hot spring, traffic FROM parks WHERE id =1")
     parks_info = c.fetchone()
@@ -19,6 +22,17 @@ def dbtest():
 def add():
     return render_template("add.html")
 
+@app.route("/add", methods = ["POST"])
+def add_post():
+    task = request.form. get("name")
+    task = request.form. get("address")
+    task = request.form. get("image")
+    con = sqlite3.connect("卒業制作.db")
+    c = con.cursor()
+    c.execute("INSERT INTO parks VALUES (null,?,?,?)", (task,))
+    con.commit()
+    con.close()
+    return redirect("/")
 
 
 if __name__ == "__main__":
