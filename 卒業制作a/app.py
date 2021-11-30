4a701f7fc841e8aa3a511afc76b751850b49b69a
fuma <jisfteteos@gmail.com>
2021-11-30 22:18:56 +0900

    作業終了

--- 卒業制作a/app.py
+++ 卒業制作a/app.py
@@ -1,13 +1,16 @@
 import os
 import sqlite3
-from flask import Flask, render_template, request, redirect, g, url_for, abort, flash
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "./static/img/"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config.from_object(__name__)
+# UPLOAD_FOLDER = "./static/img/"
+# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+# app.config.from_object(__name__)
+
+
+
 @app.route("/")
 def hello():
     return "<p>ありがとう</p>"
@@ -16,7 +19,7 @@ def hello():
 def dbtest():
     con = sqlite3.connect("卒業制作.db")
     c = con.cursor()
-    c.execute("SELECT * FROM parks WHERE id =3")
+    c.execute("SELECT * FROM parks WHERE id =1")
     parks_info = c.fetchone()
     c.close()
     print(parks_info)
@@ -32,7 +35,15 @@ def add_post():
     # id  = request.form.get("id")
     name = request.form.get("name")
     address = request.form.get("address")
-    image = request.form.get("image")
+    image = request.files["image"]
+    if not image.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        return 'png,jpg,jpeg形式のファイルを選択してください'
+    save_path = get_save_path()
+    print(save_path)
+    filename = image.filename
+    image.save(os.path.join(save_path,filename))
+    print(filename)
+
     parking  = request.form.get("parking")
     toilet  = request.form.get("toilet")
     playset  = request.form.get("playset")
@@ -47,10 +58,16 @@ def add_post():
 
     con = sqlite3.connect("卒業制作.db")
     c = con.cursor() 
-    c.execute("insert into parks values(null,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (name, address, image, comment, parking, toilet, playset, convenience, history, vending, plaza, water, hot_spring, traffic))
+    c.execute("insert into parks values(null,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (name, address, comment, parking, toilet, playset, convenience, history, vending, plaza, water, hot_spring, traffic, filename))
+    # c.execute("update parks set image = ? where id=?", (filename,parks_id))
     con.commit()
     con.close()
     return redirect("/")
+
+
+def get_save_path():
+    path_dir = "./static/img"
+    return path_dir
 
 
 if __name__ == "__main__":
